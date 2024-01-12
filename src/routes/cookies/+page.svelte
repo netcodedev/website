89ea5f89822ca2870684ba3fa476585ef72b8ad7
netcodedev <benmondini@netcode.dev>
2024-01-12 17:01:51 +0100

    Added footer and policy pages

---
+++ src/routes/cookies/+page.svelte
@@ -0,0 +1,30 @@
+<section id="header" class="flex flex-col items-center justify-center gap-4 p-4 text-white">
+    <h1 class="text-7xl sm:text-8xl">Cookies</h1>
+</section>
+<section class="border rounded-lg m-4 p-4 w-full max-w-[1000px] mx-auto">
+    <h1>Cookies</h1>
+    <span>
+        This site uses no cookies lol
+    </span>
+</section>
+
+<style lang="postcss">
+    #header {
+        background-image: linear-gradient(
+			to right bottom,
+			#c2886a,
+			#ae7a66,
+			#9a6c61,
+			#84605b,
+			#6e5453,
+			#634d4e,
+			#574649,
+			#4c4043,
+			#483c41,
+			#43383e,
+			#3e353c,
+			#39313a
+		);
+        min-height: 250px;
+    }
+</style>
---
+++ src/routes/terms/+page.svelte
@@ -0,0 +1,38 @@
+<section id="header" class="flex flex-col items-center justify-center gap-4 p-4 text-white">
+    <h1 class="text-7xl sm:text-8xl">Terms and Conditions</h1>
+    <p class="text-center">
+        a monitoring-management tool
+    </p>
+</section>
+<section class="border rounded-lg m-4 p-4 w-full max-w-[1000px] mx-auto">
+    <h1>Terms</h1>
+    <span>
+    Thank you for using our ("netcode") Services. Those are brought to you by Benedict Mihm, Im Schützenrech 7, 55494 Rheinböllen, Germany.<br/>
+    <br/>
+    By using our Services, you are agreeing to these <a href="/terms">Terms and Conditions</a> and the <a href="/privacy">Privacy Policies</a>.<br/>
+    <br/>
+    Some Products and Services may have additional Terms or Privacy Policies. Those will be linked on the Services Sites. By using Services, which use aditional Terms or Privacy Policies, you are agreeing to these. If you don't agree
+    to the provisions, you are not allowed to use them.
+    </span>
+</section>
+
+<style lang="postcss">
+    #header {
+        background-image: linear-gradient(
+			to right bottom,
+			#c2886a,
+			#ae7a66,
+			#9a6c61,
+			#84605b,
+			#6e5453,
+			#634d4e,
+			#574649,
+			#4c4043,
+			#483c41,
+			#43383e,
+			#3e353c,
+			#39313a
+		);
+        min-height: 250px;
+    }
+</style>
